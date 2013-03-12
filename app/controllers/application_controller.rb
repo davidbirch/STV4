ad3ab0b69d8b2376a4a82e5e908081c480be9cc4
David Birch <birch.dave@gmail.com>
2013-03-12 10:57:14 +0000

    split out basic auth for dev/test/prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,13 +20,15 @@ class ApplicationController < ActionController::Base
   protected
     def authenticate
       basic_auth_yaml = YAML.load_file(File.expand_path("#{Rails.root}/config/basic_auth.yml", __FILE__))
+      basic_auth_config = basic_auth_yaml[Rails.env]
+      
       
       # this is just for debudgging purposes
       #log = Logger.new(File.expand_path("#{Rails.root}/log/controller.log", __FILE__))
       #log.info("The username is #{basic_auth_yaml["username"]}")
       
       authenticate_or_request_with_http_basic do |username, password|
-        username == basic_auth_yaml["username"] && password == basic_auth_yaml["password"]
+        username == basic_auth_config["username"] && password == basic_auth_config["password"]
       end
     end
    
