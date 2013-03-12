9e4bddd13312892a8312ceab988f1aead74827b6
David Birch <birch.dave@gmail.com>
2013-03-12 12:13:23 +0000

    moved u/p to yml file for now

--- spec/support/auth_helper.rb
+++ spec/support/auth_helper.rb
@@ -1,8 +1,9 @@
 module AuthHelper
   def http_login
-    user = 'test'
-    pw = 'test'
-    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)
+    basic_auth_yaml = YAML.load_file(File.expand_path("#{Rails.root}/config/basic_auth.yml", __FILE__))
+    basic_auth_config = basic_auth_yaml[Rails.env]
+      
+    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(basic_auth_config["username"],basic_auth_config["password"])
   end
 end
  
@@ -14,8 +15,10 @@ module AuthRequestHelper
   #
   def http_login
     @env ||= {}
-    user = 'test'
-    pw = 'test'
-    @env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)
+    
+    basic_auth_yaml = YAML.load_file(File.expand_path("#{Rails.root}/config/basic_auth.yml", __FILE__))
+    basic_auth_config = basic_auth_yaml[Rails.env]
+    
+    @env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(basic_auth_config["username"],basic_auth_config["password"])
   end
 end
