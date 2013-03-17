e6d9f1c6923b7ca6568ebd2b8e9ed6a3083a2608
David Birch <birch.dave@gmail.com>
2013-03-17 04:07:10 +0000

    add back disabled production features

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,14 +8,14 @@
 # set :output, "/path/to/my/cron_log.log"
 #
 
-every 1.day, :at => '4:00 am' do
+every 1.day, :at => '4:00 pm' do
   command "perl /home/yakadmin/get_xmltv_guide_data.pl >> /home/yakadmin/log/cron.log"
 end
 
-every 1.day, :at => '5:00 am' do
+every 1.day, :at => '5:00 pm' do
   command "perl /home/yakadmin/load_raw_guide_data.pl >> /home/yakadmin/log/cron.log"
 end
 
 every 3.hours do
-  runner 'NightlyJob.new', :environment => 'development' 
+  runner 'NightlyJob.new', :environment => 'production' 
 end
